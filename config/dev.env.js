108cce691afe6da1131b65667961acc89c9fe045
Ashutosh Mishra <149000226+ashutoshm97@users.noreply.github.com>
2025-06-13 00:44:29 -0700

    Update dev.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  VUE_APP_CESIUM_TOKEN: JSON.stringify(process.env.VUE_APP_CESIUM_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNGRkZTRmMy00ODRlLTRhOWYtYWI4NC0wNDc3MmEwMjFjNDAiLCJpZCI6MzEwMTYxLCJpYXQiOjE3NDk1MTEwNDh9.5iNkluLMkGTozHLh1TjCbbVpT8zMnxlwIdtpySZ56-Q')
+  VUE_APP_CESIUM_TOKEN: JSON.stringify(process.env.VUE_APP_CESIUM_TOKEN || '')
 })
